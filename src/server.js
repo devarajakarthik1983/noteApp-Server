a79a5939ae4acd8ac4ed7461e799815b0b20cea5
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-06-20 21:11:55 -0400

    added route for get posts and get posts by id

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('./db/mongoose');
 
 const Notes = require('./models/notes')
@@ -7,6 +9,8 @@ const app = express();
 const port = process.env.POST || 3001;
 
 app.use(express.json());
+
+//post notes
 
 app.post('/notes' , (req , res)=>{
     const notes = {
@@ -19,12 +23,48 @@ app.post('/notes' , (req , res)=>{
     const newNotes = new Notes(notes);
 
     newNotes.save().then(()=>{
-        res.status(200).send(notes);
+        res.status(201).send(notes);
     }).catch((e)=>{
         res.status(400).send(e);
     })
     
 })
+
+//get all notes
+
+app.get('/notes' , (req , res)=>{
+
+    Notes.find({}).then((notes)=>{
+        res.status(201).send(notes)
+    }).catch((e) =>{
+        res.status(500).send(e);
+    });
+});
+
+//get notes by id
+
+app.get('/notes/:id' , (req,res)=>{
+    const _id =  req.params.id;
+
+    const _newId = mongoose.Types.ObjectId.isValid(_id);
+    console.log(_newId);
+
+    if(_newId) {
+        Notes.findById(_id).then((note)=>{
+            if(!note) {
+                res.status(404).send('Unable to find the user');
+            }
+            res.status(200).send(note);
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }else{
+        res.status(400).send('Bad ObjectID');
+         }
+})
+
+
+
 
 app.listen(port , ()=>{
     console.log('Server is running in port ' + port);
